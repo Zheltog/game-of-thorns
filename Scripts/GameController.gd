ee672f99d8fa9b7bdaf1d32d73686d9005216bd4
Daniil Beloglazov <zheltogsan@gmail.com>
2024-01-25 21:10:10 +0500

    double attack on every 4th round
    double attack on every 4th round

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -13,11 +13,11 @@ var _current_thorns_num: int
 var _round_number: int = 0
 var _round_value_label: Label
 var _thorns_value_label: Label
-var _next_attack_value_label: Label
+var _next_attacks_value_label: Label
 var _message_label: Label
 var _field: Field
 var _menu: ColorRect
-var _next_direction: Field.AttackDirection = Field.AttackDirection.UP
+var _next_attacks: Array[Field.AttackDirection]
 var _all_directions: Array
 var _can_move: bool
 
@@ -26,7 +26,7 @@ func _ready():
 	EventBus.no_cells_for_thorns.connect(_finish_round)
 	_round_value_label = get_node("StatusPanel/RoundValueLabel")
 	_thorns_value_label = get_node("StatusPanel/ThornsValueLabel")
-	_next_attack_value_label = get_node("StatusPanel/NextAttackValueLabel")
+	_next_attacks_value_label = get_node("StatusPanel/NextAttacksValueLabel")
 	_message_label = get_node("MenuPanel/MessageLabel")
 	_field = get_node("Field")
 	_menu = get_node("MenuPanel")
@@ -41,7 +41,7 @@ func _open_menu(text: String):
 func _new_game():
 	_round_value_label.text = ""
 	_thorns_value_label.text = ""
-	_next_attack_value_label.text = ""
+	_next_attacks_value_label.text = ""
 	_field.show()
 	_field.init(cells_num_hor, cells_num_ver, remove_all_pause_sec, remove_thorn_pause_sec)
 	_field.reset_cells_thorned()
@@ -70,13 +70,17 @@ func _init_directions_array():
 		
 func _finish_round():
 	_can_move = false
-	await _field.attack_on_salami(_next_direction)
+	for attack in _next_attacks:
+		await _field.attack_on_salami(attack)
 	_next_round()
 	
-func _set_random_next_attack_direction():
-	var random_index: int = randi() % (_all_directions.size() - 1)
-	var random_direction: Field.AttackDirection = Field.AttackDirection.get(_all_directions[random_index])
-	_next_direction = random_direction
+func _generate_next_attacks():
+	var attacks_num = 2 if _round_number % 4 == 0 else 1
+	_next_attacks.resize(attacks_num)
+	for i in range(attacks_num):
+		var random_index: int = randi() % (_all_directions.size() - 1)
+		var random_direction: Field.AttackDirection = Field.AttackDirection.get(_all_directions[random_index])
+		_next_attacks[i] = random_direction
 	
 func _next_round():
 	_current_thorns_num = initial_thorns_num - _round_number
@@ -85,7 +89,7 @@ func _next_round():
 		_open_menu(str("game over\nno salami remaining\nyou reached round ", _round_number))
 		return
 	_round_number = _round_number + 1
-	_set_random_next_attack_direction()
+	_generate_next_attacks()
 	_update_statuses()
 	if _current_thorns_num != 0:
 		_can_move = true
@@ -94,10 +98,17 @@ func _next_round():
 		_finish_round()
 
 func _update_statuses():
-	var next_direction_str: String = Field.AttackDirection.keys()[_next_direction]
 	_round_value_label.text = str(_round_number)
 	_thorns_value_label.text = str(_current_thorns_num)
-	_next_attack_value_label.text = next_direction_str
+	var next_attacks_str = ""
+	for i in range(_next_attacks.size()):
+		var attack = _next_attacks[i]
+		var attack_str = (Field.AttackDirection.keys()[attack] as String).substr(0, 1)
+		if i == 0:
+			next_attacks_str += attack_str
+		else:
+			next_attacks_str += "+" + attack_str
+	_next_attacks_value_label.text = next_attacks_str
 
 func _on_new_game_button_pressed():
 	_new_game()
