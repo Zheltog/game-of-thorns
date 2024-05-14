bab61667ffc1aa6286cf8629123fb535cb7ecfde
Daniil Beloglazov <zheltogsan@gmail.com>
2024-05-14 20:59:39 +0500

    ui fixes

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -4,13 +4,16 @@ var save_data: SaveData
 var lang_options: OptionButton
 var lang_button: Button
 var lang_popup: PopupMenu
+var ads_check_box: CheckBox
 
 func _ready():
 	save_data = SaveManager.load()
-	($BasePanel/LangOptionButton).button_pressed = save_data.ads_enabled
 	lang_options = get_node("BasePanel/LangOptionButton")
 	lang_button = get_node("BasePanel/LangButton")
 	lang_popup = get_node("BasePanel/LangPopupMenu")
+	ads_check_box = get_node("BasePanel/AdsCheckBox")
+	ads_check_box.button_pressed = save_data.ads_enabled
+	
 	_init_lang_options()
 	_localize_stuff()
 
@@ -48,13 +51,14 @@ func _on_lang_button_pressed():
 	lang_button.hide()
 	lang_popup.popup(Rect2i(lang_button.position.x, lang_button.position.y, lang_button.size.x, lang_button.size.y))
 
-
 func _on_lang_popup_menu_index_pressed(index):
 	var lang = ($BasePanel/LangOptionButton).get_item_text(index)
 	save_data.lang = lang
 	SaveManager.save(save_data)
 	LocalManager.set_localization(lang)
 	_localize_stuff()
-	lang_popup.hide()
 	lang_button.text = lang
+	lang_popup.hide()
+
+func _on_lang_popup_menu_popup_hide():
 	lang_button.show()
